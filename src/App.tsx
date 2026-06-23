b141277ee54954edb541fb528138bb1b7167ff5c
namandisafari <namandisafari@gmail.com>
2026-06-23 22:35:42 +0300

    fix: correct JSX tag nesting in App.tsx
    - BrowserRouter closes AFTER KeyboardShortcutsProvider (not before)
    - Fixes build error: mismatched opening/closing tags
    - Build verified successful with vite build

--- src/App.tsx
+++ src/App.tsx
@@ -246,8 +246,8 @@ const App = () => {
           <Sonner />
           <PWAInstallPrompt />
           <InstallPrompt />
-            <BrowserRouter>
-          <KeyboardShortcutsProvider>
+          <BrowserRouter>
+            <KeyboardShortcutsProvider>
               <Routes>
               <Route path="/" element={<OnboardingGate />} />
               <Route path="/home" element={<PWAHome />} />
@@ -537,8 +537,8 @@ const App = () => {
               {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
               <Route path="*" element={<NotFound />} />
             </Routes>
-          </BrowserRouter>
           </KeyboardShortcutsProvider>
+          </BrowserRouter>
         </TooltipProvider>
       </QueryClientProvider>
     </ThemeProvider>
